b69418a72a1a18ff395e429043ba65819cb21613
Claudio Rotolo <claudio.work69@gmail.com>
2022-06-04 22:57:10 +0200

    chore: commented std endpoints support

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -9,14 +9,14 @@ class WebsiteUser(HttpUser):
     def py_conn_pool(self):
         self.client.get(url="http://py_pool:8000/py_pool")
 
-    @task
-    def py_std_conn(self):
-        self.client.get(url="http://py_std:8001/py_std")
+    # @task
+    # def py_std_conn(self):
+    #     self.client.get(url="http://py_std:8001/py_std")
 
     @task
     def go_conn_pool(self):
         self.client.get(url="http://go_pool:8002/go_pool")
 
-    @task
-    def go_std_conn(self):
-        self.client.get(url="http://go_std:8003/go_std")
+    # @task
+    # def go_std_conn(self):
+    #     self.client.get(url="http://go_std:8003/go_std")
